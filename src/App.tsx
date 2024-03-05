f3d16abf598d8c64fb6fb692797c1d65c888ee83
FIRST_NAME LAST_NAME <farahmehr1@gmail.com>
2024-03-05 12:27:49 -0500

    uodate 12.27

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import Profile from "./Components/Profile";
 import LinkButtons from "./Components/LinkButtons";
 import ContactForm from "./Components/Form"
-import Greeting from "./Components/Greeting";
+
 
 const App =() => {
-  const handleClick = () => {
-    console.log("Clicked on Button");
-  };
+ 
 
   return ( 
     <div className="main">
