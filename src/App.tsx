ca7d280ac57c53baf1fb5507b406716f9efd7a96
FIRST_NAME LAST_NAME <farahmehr1@gmail.com>
2024-03-04 21:07:32 -0500

    asignment updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import Profile from "./Components/Profile";
-import Contact from "./Components/Contact";
 import LinkButtons from "./Components/LinkButtons";
 import ContactForm from "./Components/Form"
 import Greeting from "./Components/Greeting";
@@ -13,12 +12,12 @@ const App =() => {
     <div className="main">
       <Profile/>
       <LinkButtons/>
-      <Contact/>
       <ContactForm/>
       <>
       <Greeting name="fara" age={30} isMale={false} handleClick={handleClick} />
       </>
     </div>
+
   );
 };
 
