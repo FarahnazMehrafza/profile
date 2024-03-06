2a56c03cd57b5aaea5e6615f1b254f7a03619a55
FIRST_NAME LAST_NAME <farahmehr1@gmail.com>
2024-03-05 20:17:36 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ import ProfileHeading, { ProfileText, CoverPhoto, ProfilePhoto } from './Compone
 const App = () => {
   return ( 
     <div className="main">
-         <CoverPhoto url="../src/assets/cover.png" />
-         <ProfilePhoto url="..//src/assets/farah.png"/>
+         <CoverPhoto url="../public/cover.png" />
+         <ProfilePhoto url="../public/farah.png"/>
          <ProfileHeading Titr=" Farahnaz Mehrafza " />
          <ProfileHeading Titr=" Frontend Developer JS Reac" />
 
